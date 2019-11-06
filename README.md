c0a14cde3029a9a69a5d3cbbc10580f57a6038f0
Megan Corletti <corlettim@allegheny.edu>
2019-11-06 13:08:44 -0500

    add jbe lab

---
+++ README.md
@@ -0,0 +1,2 @@
+# computer-science-201-fall-2019-lab-1
+computer-science-201-fall-2019-lab-1
---
+++ myimages/_data/Lab1.java
@@ -0,0 +1,15 @@
+// Megan Corletti, This work is mine unless otherwise cited.
+
+public class Lab1 {
+    public static void main(String[] args) {
+        int i = 5, j = 6, k = 127, l = 128, m = 255,
+            n = 32767, o = 32768;
+        System.out.println("i="+i);
+        System.out.println("j="+j);
+        System.out.println("k="+k);
+        System.out.println("l="+l);
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+        System.out.println("o="+o);
+    }
+}
